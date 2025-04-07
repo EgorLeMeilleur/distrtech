5d62fc5e1ff69dc37cfdde3768060f1ea4fa0462
egorlemeilleur <egorpokumin@gmail.com>
2025-04-07 12:17:26 +0500

    lol

--- task2/exporter.py
+++ task2/exporter.py
@@ -25,35 +25,24 @@ def main():
     if args.mode == 'socket':
         socket_config = config["connection"]["socket"]
         comm_data = SocketCommunication(socket_config["host_data"], socket_config["port"])
-        
-        rsa_public_key = comm_data.receive_data(timeout=10)
-        if not rsa_public_key:
-            print("Нет данных или соединение прервано. Завершение импорта.")
-            return
-        
-        print("Получен публичный ключ RSA")
-        pem_public_key = json.loads(rsa_public_key)["public_key"]
-        rsa_key = serialization.load_pem_public_key(pem_public_key.encode('utf-8'), backend=default_backend())
-
-        aes_key = generate_aes_key()
-        encrypted_aes_key = encrypt_with_rsa(rsa_key, aes_key)
+        comm_key = SocketCommunication(socket_config["host_key"], socket_config["port"])
 
     elif args.mode == 'queue':
         queue_config = config["connection"]["queue"]
         comm_data = QueueCommunication(queue_config, queue_config["host_data"], queue_config["queue_data"], queue_config["exchange_data"], queue_config["routing_data"])
         comm_key = QueueCommunication(queue_config, queue_config["host_key"], queue_config["queue_key"], queue_config["exchange_key"], queue_config["routing_key"])
 
-        rsa_public_key = comm_key.receive_data(timeout=10)
-        if not rsa_public_key:
-            print("Нет данных или соединение прервано. Завершение импорта.")
-            return
-        
-        print("Получен публичный ключ RSA")
-        pem_public_key = json.loads(rsa_public_key)["public_key"]
-        rsa_key = serialization.load_pem_public_key(pem_public_key.encode('utf-8'), backend=default_backend())
+    rsa_public_key = comm_key.receive_data(timeout=10)
+    if not rsa_public_key:
+        print("Нет данных или соединение прервано. Завершение импорта.")
+        return
+    
+    print("Получен публичный ключ RSA")
+    pem_public_key = json.loads(rsa_public_key)["public_key"]
+    rsa_key = serialization.load_pem_public_key(pem_public_key.encode('utf-8'), backend=default_backend())
 
-        aes_key = generate_aes_key()
-        encrypted_aes_key = encrypt_with_rsa(rsa_key, aes_key)
+    aes_key = generate_aes_key()
+    encrypted_aes_key = encrypt_with_rsa(rsa_key, aes_key)
         
     comm_data.send_data(encrypted_aes_key)
     print("Отправлен ключ AES")
